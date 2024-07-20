d6f65a4cb0a8b017722e127f72fc47a0ae659f80
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2024-07-20 10:18:37 +0530

    Db.js

--- database/Db.js
+++ database/Db.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'BlogAllNotes',
-    password: 'Sachin@414',
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'BlogAllNotes',
+    // password: 'Sachin@414',
+    DB_USER:'my_blog_database_8oh3_user',
+    DB_HOST:'dpg-cq8chjij1k6c73chv56g-a',
+    DB_DATABASE:'my_blog_database_8oh3',
+    DB_PASSWORD:'nSDyJn7S3wjPDS4bnHjtXdEurqommpFL',
     port: 5432,
     jwtSecret: 'your_secure_jwt_secret',
 
